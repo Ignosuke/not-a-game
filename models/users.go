023459b9cd73accef97053978cc80e3cd2ba25da
Iqna <ignabruno2021@gmail.com>
2023-03-11 18:49:50 -0300

    update

--- models/users.go
+++ models/users.go
@@ -6,6 +6,7 @@ import (
 
 type Users struct {
 	gorm.Model
+	ID    int64  `json:"id" gorm:"primary_key;auto_increment"`
 	Fname string `json:"fname"`
 	Lname string `json:"lname"`
 	Uname string `json:"uname"`
